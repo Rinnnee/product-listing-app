6cb75144a621bc3e789bb87d63fab6d48e9892c8
Rinnnee <thanapon.h24@gmail.com>
2026-02-16 17:01:41 +0700

    refactor: update productCard

--- components/productCard/ProductCardSkeleton.tsx
+++ components/productCard/ProductCardSkeleton.tsx
@@ -13,19 +13,15 @@ import {
   
         <CardHeader className="flex flex-col gap-3">
   
-          {/* Brand + Category */}
           <div className="flex justify-between w-full">
             <Skeleton className="h-4 w-20 animate-pulse" />
             <Skeleton className="h-5 w-16 rounded-full" />
           </div>
   
-          {/* Title */}
           <Skeleton className="h-5 w-3/4" />
   
-          {/* Rating */}
           <Skeleton className="h-4 w-24" />
   
-          {/* Price */}
           <div className="flex gap-2">
             <Skeleton className="h-6 w-16" />
             
